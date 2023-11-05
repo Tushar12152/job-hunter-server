011c841ed05b7146c726e9c2c02fa1df9ea3c604
Tushar12152 <tusharimran470@gmail.com>
2023-11-05 03:47:51 -0700

    users info post and get to database

--- index.js
+++ index.js
@@ -32,10 +32,35 @@ async function run() {
     await client.connect();
 
      const jobCollection=client.db("jobdb").collection('jobs')
+     const userCollection=client.db("usersdb").collection('users')
+
+
+
+//usersdb
+
+app.post('/users',async(req,res)=>{
+    const user=req.body;
+    // console.log(user);
+  const result=await userCollection.insertOne(user)
+  res.send(result)
+
+
+})
+
+
+app.get('/users',async(req,res)=>{
+    const result=await userCollection.find().toArray()
+          res.send(result) 
+  })
 
 
 
 
+
+
+
+
+//jobsDB
      app.post('/jobs',async(req,res)=>{
           const job=req.body;
         //   console.log(job);
