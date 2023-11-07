f389c8fb34086bdb1beca5ee966ed941f16c7113
Tushar12152 <tusharimran470@gmail.com>
2023-11-06 18:52:30 -0700

    all application  get from database is successfully compleate

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const cors = require('cors');
+// const cookieParser = require('cookie-parser')
+// const jwt = require('jsonwebtoken');
+
 require('dotenv').config()
 const app=express()
 const port=process.env.PORT ||5002;
@@ -9,6 +12,8 @@ const port=process.env.PORT ||5002;
 //middleWare
 app.use(cors())
 app.use(express.json())
+// app.use(cookieParser)
+
 
 
 
@@ -26,6 +31,7 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -38,29 +44,68 @@ async function run() {
 
 
 
+//jwt
+
+// app.post("/jwt",async(req,res)=>{
+//    const user=req.body;
+//    const token=jwt.sign(user,process.env.secret,{expiresIn:"1h"})
+//    res.cookie('token',token,{
+//        httpOnly:true,
+//        secure:true,
+//        sameSite:'none'
+//    })
+//    .send({success:true})
+// })
+
+
+// app.post('/logout',async(req,res)=>{
+//     const user=req.body;
+//     console.log('logging out',user);
+//     res.clearCookie("token",{maxAge:0})
+//     .send({success:true})
+// })
+
+
+
+
+
+
+
+
+
+
      //applicationDB
 
 app.post("/applications",async(req,res)=>{
      const application= req.body;
-     console.log(application);
+    //  console.log(application);
      const result=await ApplicationCollection.insertOne(application)
      res.send(result)
 
 })
 
 
+// app.get("/applications/:id",async(req,res)=>{
+//   const id=req.params.id
+//   console.log(id);
+//    const query={_id:new ObjectId(id)}
+//    const result=await ApplicationCollection.findOne(query)
+//    res.send(result)
+
+// })
+
+
+
 
 app.get('/applications',async(req,res)=>{
+  // console.log(req.cookies);
   const result=await ApplicationCollection.find().toArray()
         res.send(result) 
 })
 
-// app.delete("/applications/:id",async(req,res)=>{
-//        const id=req.params.id
-//        const query ={_id:new ObjectId(id)}
 
-//        const result=await 
-// })
+
+
 
 
 
@@ -109,7 +154,7 @@ app.get('/users',async(req,res)=>{
 
      })
 
-     
+
 
      app.delete("/jobs/:id",async(req,res)=>{
             const id=req.params.id;
@@ -117,6 +162,31 @@ app.get('/users',async(req,res)=>{
             const query={_id:new ObjectId(id)}
             const result=await jobCollection.deleteOne(query)
             res.send(result)
+     })
+
+     app.put("/jobs/:id",async(req,res)=>{
+          
+
+      const id=req.params.id;
+      const filter={_id:new ObjectId(id)};
+      const options={upsert:true};
+      const updatedJob=req.body
+      const job={
+        $set:{
+          photo:updatedJob.photo,
+          title:updatedJob.title,
+          UserName:updatedJob.UserName,
+          category:updatedJob.category,
+          deadLine:updatedJob.deadLine,
+          salary:updatedJob.salary,
+          description:updatedJob.description,
+          applicants:updatedJob.applicants
+        }
+      }
+    
+      const result=await jobCollection.updateOne(filter,job,options)
+      res.send(result)
+
      })
  
 
